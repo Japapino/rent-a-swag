61690cb5d486b4d0df775c5ab71d2268c6893a1b
Japapino <stefan.murakami@gmail.com>
2018-05-16 14:33:33 -0500

    passed shouldDisplayOneProduct test in browseMVCTest

--- src/test/java/rentaswag/website/browseMVCTest.java
+++ src/test/java/rentaswag/website/browseMVCTest.java
@@ -4,6 +4,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import javax.annotation.Resource;
+import javax.net.ssl.SSLEngineResult.Status;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -11,6 +12,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import static org.mockito.Mockito.when;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(BrowseController.class)
@@ -25,5 +27,12 @@ public class BrowseMVCTest {
 	@Test
 	public void shouldDisplayProducts() throws Exception {
 		mvc.perform(get("/products")).andExpect(status().isOk()); 
+	}
+	
+	@Test
+	public void shouldDisplayOneProduct() throws Exception {
+		when(productRepo.findOne(1L)).thenReturn(new Product("test"));
+		
+		mvc.perform(get("/products/1")).andExpect(status().isOk());
 	}
 }
